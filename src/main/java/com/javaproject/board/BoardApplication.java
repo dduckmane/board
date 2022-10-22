4dd869c8932fb0928d2428ca98a4e37c5f9682f0
minsung <kms199719@naver.com>
2022-10-22 23:51:48 +0900

    2022-10-22 23:51 엔티티 설정

--- src/main/java/com/javaproject/board/BoardApplication.java
+++ src/main/java/com/javaproject/board/BoardApplication.java
@@ -2,6 +2,11 @@ package com.javaproject.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @SpringBootApplication
 public class BoardApplication {
@@ -9,5 +14,8 @@ public class BoardApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
 	}
-
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		return () -> Optional.of(UUID.randomUUID().toString());
+	}
 }
